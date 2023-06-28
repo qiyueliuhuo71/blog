d115d463be141382d8c1b66c5f4b6033e33d54df
qiyueliuhuo <1191682577@qq.com>
2023-06-28 19:22:47 +0800

    dsds

--- git_test2.php
+++ git_test2.php
@@ -3,7 +3,7 @@
  * @Author: qiyueliuhuo 1191682577@qq.com
  * @Date: 2022-08-09 09:19:30
  * @LastEditors: qiyueliuhuo 1191682577@qq.com
- * @LastEditTime: 2023-06-28 19:18:58
+ * @LastEditTime: 2023-06-28 19:20:11
  * @FilePath: /learngit/git_test1.php
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -12,6 +12,13 @@ echo 'aaa';
 //cs
 echo '双11';
 echo '230628';
+print_r(['cs@cs_line'=>"======".__LINE__."======\n",'data'=>[]]);
+print_r(['cs@cs_line'=>"======".__LINE__."======\n",'data'=>[]]);
+print_r(['cs@cs_line'=>"======".__LINE__."======\n",'data'=>[]]);
+print_r(['cs@cs_line'=>"======".__LINE__."======\n",'data'=>[]]);
+print_r(['cs@cs_line'=>"======".__LINE__."======\n",'data'=>[]]);
+print_r(['cs@cs_line'=>"======".__LINE__."======\n",'data'=>[]]);
+print_r(['cs@cs_line'=>"======".__LINE__."======\n",'data'=>[]]);
 print_r(['cs@cs_line'=>"======".__LINE__."======\n",'data'=>[]]);
 print_r(['cs@cs_line'=>"======".__LINE__."======\n",'data'=>[]]);
 print_r(['cs@cs_line'=>"======".__LINE__."======\n",'data'=>[]]);
