895dba9d992ba99fb919c3d3b91b15766acb7b18
qiyueliuhuo <1191682577@qq.com>
2023-06-29 15:10:22 +0800

    w2

--- git_test2.php
+++ git_test2.php
@@ -3,14 +3,12 @@
  * @Author: qiyueliuhuo 1191682577@qq.com
  * @Date: 2022-08-09 09:19:30
  * @LastEditors: qiyueliuhuo 1191682577@qq.com
- * @LastEditTime: 2023-06-29 15:07:53
+ * @LastEditTime: 2023-06-29 15:10:04
  * @FilePath: /learngit/git_test1.php
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 
 //cs
-print_r(['cs@cs_line'=>"======".__LINE__."======\n",'data'=>['wjj1']]);
-print_r(['cs@cs_line'=>"======".__LINE__."======\n",'data'=>['wjj2']]);
-print_r(['cs@cs_line'=>"======".__LINE__."======\n",'data'=>['wjj3']]);
+print_r(['cs@cs_line'=>"======".__LINE__."======\n",'data'=>['git']]);
 die;
 ?>
